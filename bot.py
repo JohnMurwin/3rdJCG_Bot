bdfb8c57b6d3692eab296832b88a0e8e4c5c4f70
JMurwin <john.murwin@gmail.com>
2020-04-07 13:56:49 -0400

    Introducing Cogs
    - Seperated code out into indpendent Cog Files
    - maint boty.py will now load those independent files automatically so we can work on them seperately

--- bot.py
+++ bot.py
@@ -1,19 +1,35 @@
 # 3rd Joint Combat Group Discord Bot
 # Codename: "JR"
+#By: J. Murwin, W. Alphin
 
-#J. Murwin, W. Alphin
+#--CONFIG--
+token = "NTg5OTk3MTE3MTIxMjMyOTM3.XoyVzw.N5IV-mpG-edUdDr9VbTwDMhVClM" 
+prefix = "!"
 
+#Discord Setup
 import discord
+import os
 from discord.ext import commands
 
 #Command Prefix Setup
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = prefix)
 
-# EVENTS #
-@client.event
-async def on_ready():
-    print ('Jr is Online')
+# LOAD COGS #
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+# UNLOAD COGS #
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
 
 
-#RUN MOFO #
-client.run('NTg5OTk3MTE3MTIxMjMyOTM3.XoyVzw.N5IV-mpG-edUdDr9VbTwDMhVClM')
+# AUTO-LOAD COGS#
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}') # load the extension but splice off the .py
+
+
+# RUN MOFO #
+client.run(token)
